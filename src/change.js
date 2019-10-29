e648e5788d03ed893bc17067b74e1ab7e734572c
Danielle Kang <daniellekang24@gmail.com>
2019-10-29 10:50:01 -0400

    initial commit

--- src/change.js
+++ src/change.js
@@ -3,12 +3,36 @@ const readlineSync = require("readline-sync");
 const MIN = 0.01;
 const MAX = 0.99;
 
+
+const QUARTER_VALUE = 25;
+const DIME_VALUE = 10;
+const NICKEL_VALUE = 5;
+const PENNY_VALUE = 1;
+
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
+
+let quarterNumber = 0;
+let dimeNumber = 0;
+let nickelNumber = 0;
+let pennyNumber = 0;
+
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("Invalid.");
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("Invalid.");
 } else {
-    // write your code for making change here
+      let numberCents = amount * 100;
+      quarterNumber = Math.floor(numberCents/QUARTER_VALUE);
+      numberCents = (numberCents%QUARTER_VALUE);
+
+      dimeNumber = Math.floor(numberCents/DIME_VALUE);
+      numberCents = (numberCents % DIME_VALUE);
+
+      nickelNumber = Math.floor(numberCents/NICKEL_VALUE);
+      numberCents = (numberCents % DIME_VALUE);
+
+      pennyNumber = numberCents/PENNY_VALUE;
+
+    console.log(`\n${quarterNumber} quarters, ${dimeNumber} dimes, ${nickelNumber}  nickels, and ${pennyNumber} pennies.`)
 }
